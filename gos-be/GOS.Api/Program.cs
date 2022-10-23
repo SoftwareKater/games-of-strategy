b4f19ea920e1bef392130a3bfd9251812f2bfe97
SoftwareKater <softwarekater@gmail.com>
2022-10-23 21:55:08 +0200

    try to add nswag

--- gos-be/GOS.Api/Program.cs
+++ gos-be/GOS.Api/Program.cs
@@ -8,18 +8,30 @@ services.AddSingleton<TournamentService, TournamentService>();
 
 services.AddControllers();
 
+// Register the NSwag Swagger services
+services.AddSwaggerDocument();
+
 var app = builder.Build();
 
+app.UseStaticFiles();
 if (app.Environment.IsDevelopment())
 {
+    // Register the NSwag Swagger generator and the Swagger UI middlewares
+    // Register the middleware before UseRouting()
+    app.UseOpenApi();
+    app.UseSwaggerUi3();
 }
 
-app.UseFileServer();
+// app.UseFileServer();
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
-app.UseAuthorization();
+// app.UseAuthorization();
 
-app.MapControllers();
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.Run();
